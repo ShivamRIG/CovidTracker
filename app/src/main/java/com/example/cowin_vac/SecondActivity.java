7ef259fb6c4b9db04523381df525caaac3f1ad0e
Shivam <shivamsingh8461@outlook.com>
2021-06-23 17:59:43 +0530

    added list in the next frame

--- app/src/main/java/com/example/cowin_vac/SecondActivity.java
+++ app/src/main/java/com/example/cowin_vac/SecondActivity.java
@@ -2,22 +2,27 @@ package com.example.cowin_vac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
     TextView Txtv;
@@ -27,20 +32,30 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Txtv=(TextView)findViewById(R.id.textView2);
-         data = getIntent().getStringExtra("data");
-        Txtv.setText(data);
+        data = getIntent().getStringExtra("data");
+        ListView listView =findViewById(R.id.listview);
+        ArrayList<String> info=new ArrayList<>();
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,info);
+
         RequestQueue queue;
         queue= Volley.newRequestQueue(this);
         
 // Request a string response from the provided URL.
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=363&date=20-06-2021",null, new Response.Listener<JSONArray>() {
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=363&date="+data,null, new Response.Listener<JSONObject>() {
 
                     @Override
-                    public void onResponse(JSONArray response) {
+                    public void onResponse(JSONObject response) {
                         try {
-                            Log.d("res",response.getString(2));
-                        } catch (JSONException e) {
+                            JSONObject jsonObject1 = new JSONObject(response.toString());
+                            JSONArray jsonArray = jsonObject1.getJSONArray("sessions");
+                            Log.d("Json response", "onResponse: "+jsonObject1.toString());
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                info.add(jsonObject.get("name").toString());
+                            }
+                            listView.setAdapter(arrayAdapter);
+                            } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
@@ -49,10 +64,10 @@ public class SecondActivity extends AppCompatActivity {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // TODO: Handle error
-                       Log.d("map", String.valueOf(error));
+                       Log.d("data", String.valueOf(error));
                     }
                 });
-            queue.add(jsonArrayRequest);
+            queue.add(jsonRequest);
 
 
 // Add the request to the RequestQueue.
